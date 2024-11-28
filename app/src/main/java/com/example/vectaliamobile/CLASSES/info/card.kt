4f3c5a0b1b047d58206773d0e55dc5abdafff8f4
abnderrazak-errifaouy <abdrzakrifawi@gmail.com>
2024-11-28 13:07:52 +0100

    add_piment.kt

--- app/src/main/java/com/example/vectaliamobile/CLASSES/info/card.kt
+++ app/src/main/java/com/example/vectaliamobile/CLASSES/info/card.kt
@@ -2,21 +2,28 @@ package com.example.vectaliamobile.CLASSES.info
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.vectaliamobile.CLASSES.info.Piment.piment
+import com.example.vectaliamobile.CLASSES.info.Piment.piment_Rib
 import java.time.LocalDate
+import java.util.Date
 
 class Card(
     val id: Int,
-    var active: Boolean,
     val code: Int,
     var disponible: Boolean
 ) {
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun updateActiveStatus(date: LocalDate) {
-        val currentMonth = LocalDate.now().month
-        val inputMonth = date.month
-
-        active = (currentMonth == inputMonth)
+    val listPiment = mutableListOf<piment>()
+    var active: Boolean = false
+    var tcheck = false
+    fun isActive(){
+        val thisDate = Date()
+        for ( i in listPiment){
+            if (thisDate.month == i.DatePiment.month ){
+               tcheck = true
+            }
+        }
+        if (tcheck){
+            active = true
+        }
     }
-
 }
